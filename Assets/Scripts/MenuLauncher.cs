68d84acb4f9aea64b1ad29a7d010edbcbe87a0a0
Vivian Azalia <55457999+vivianazalia@users.noreply.github.com>
2022-08-29 23:58:02 +0800

    Texture Atlas

--- Assets/Scripts/MenuLauncher.cs
+++ Assets/Scripts/MenuLauncher.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.U2D;
 
 public class MenuLauncher : MonoBehaviour
 {
     [SerializeField] private Button _playAudioButton;
     [SerializeField] private Button _playNoAudioButton;
+    [SerializeField] private SpriteAtlas _atlas;
+
     // Start is called before the first frame update
     void Start()
     {
+        _playAudioButton.GetComponent<Image>().sprite = _atlas.GetSprite("Blue gradient");
+        _playNoAudioButton.GetComponent<Image>().sprite = _atlas.GetSprite("Red gradient");
+
         _playAudioButton.onClick.RemoveAllListeners();
         _playNoAudioButton.onClick.RemoveAllListeners();
         _playAudioButton.onClick.AddListener(OnWithAudioButtonClicked);
