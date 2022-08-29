19dd5a4fdb67b339db503e95f808244eb2032ed4
vivianazalia <vivianazalia01@gmail.com>
2022-08-29 14:37:38 +0800

    Mushroom Pooling

--- Assets/Scripts/Mushroom.cs
+++ Assets/Scripts/Mushroom.cs
@@ -6,14 +6,46 @@ public class Mushroom : MonoBehaviour
 {
     public float upForce = 1f;
     public float sideForce = .1f;
-    void Start()
+
+    private float timer = 3;
+
+    private Vector3 force;
+    private Vector3 startPos;
+
+    private void OnEnable()
+    {
+        Force();
+        GetComponent<Rigidbody>().velocity = force;
+    }
+
+    private void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public void Force()
     {
         float xForce = Random.Range(-sideForce, sideForce);
-        float yForce = Random.Range(upForce/2f, upForce);
+        float yForce = Random.Range(upForce / 2f, upForce);
         float zForce = Random.Range(-sideForce, sideForce);
 
-        Vector3 force = new Vector3 (xForce, yForce, zForce);
+        force = new Vector3(xForce, yForce, zForce);
+    }
 
-        GetComponent<Rigidbody>().velocity = force;
+    public void Spawn()
+    {
+        if(timer <= 0)
+        {
+            gameObject.SetActive(false);
+            transform.position = startPos;
+            timer = 3;
+        }
+
+        timer -= Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        Spawn();
     }
 }
--- Assets/Scripts/MushroomSpawner.cs
+++ Assets/Scripts/MushroomSpawner.cs
@@ -5,8 +5,53 @@ using UnityEngine;
 public class MushroomSpawner : MonoBehaviour
 {
     public GameObject objPrefab;
+    [SerializeField] private int count;
+
+    private List<GameObject> mushrooms = new List<GameObject>();
+
+    private float timerToSpawn;
+
+    private void Start()
+    {
+        SpawnInit();
+    }
+
+    public void SpawnInit()
+    {
+        for (int i = 0; i < count; i++)
+        {
+            GameObject obj =  Instantiate(objPrefab, transform.position, Quaternion.identity);
+            obj.transform.SetParent(this.transform);
+            obj.SetActive(false);
+            mushrooms.Add(obj);
+        }
+    }
+
+    public void GetObject()
+    {
+        foreach(var m in mushrooms)
+        {
+            if (!m.activeInHierarchy)
+            {
+                m.SetActive(true);
+                return;
+            }
+        }
+    }
+
+    public void Spawn()
+    {
+        if(timerToSpawn <= 0)
+        {
+            GetObject();
+            timerToSpawn = .8f;
+        }
+        
+        timerToSpawn -= Time.deltaTime;
+    }
     
     void FixedUpdate(){
-        Instantiate(objPrefab, transform.position, Quaternion.identity);
+        //Instantiate(objPrefab, transform.position, Quaternion.identity);
+        Spawn();
     }
 }
